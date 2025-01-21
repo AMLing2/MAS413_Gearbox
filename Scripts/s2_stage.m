667cd8d7659d001fb19269ba06a6498101cdfb0b
AMLing2 <mathias.lervik@hotmail.com>
2025-01-21 16:05:08 +0100

    printing divisors properly

--- Scripts/s2_stage.m
+++ Scripts/s2_stage.m
@@ -9,37 +9,45 @@ n_out = n_1/i_tot;
 
 
 % calculations for internal gears
-% i_1 = 4; % first gear ratio of 2 stage from table 15-38, Lec2 pg12
-% i_2 = i_tot/i_1;
+ i_1 = 5; % first gear ratio of 2 stage from table 15-38, Lec2 pg12
+ i_2 = i_tot/i_1
 
 z_1_min = 19; % minimum 18-20 from teacher [# teeth]
-% z_2 = i_1 * z_1_min
-z_2 = 97;
-
-z_1_divisors = alldivisors(z_1_min)
-z_2_divisors = alldivisors(z_2)
+z_2 = round(i_1 * z_1_min);
+% z_2 = 97;
+z_1_divisors = alldivisors(z_1_min);
+z_2_divisors = alldivisors(z_2);
 stage1_divisors = intersect(z_1_divisors,z_2_divisors);
 
-if ~isempty(stage1_divisors) % greatest common denominator
-    disp(["gcd stage 2: ",gcd(z_1_min,z_2)]);
+if ~isempty(stage1_divisors)
+    disp(["stage 1 divisors: ",stage1_divisors]);
     warning("stage 1 gears are not relative prime") 
 else
-    disp(["gcd stage 2: ",gcd(z_1_min,z_2)]);
+    disp(["stage 1 divisors: ",stage1_divisors]);
     % disp("\nStage 1 gears ARE relative")
 end
 
+i_2 = i_tot/(z_2/z_1_min)
+
 z_4 = 173; % from converting i_2 to rational
 z_3 = 50;
+
+z_3 = 20;
+z_4 = round(z_3 * i_2) + 1;
 
 z_4_divisors = alldivisors(z_4);
 z_3_divisors = alldivisors(z_3);
 stage2_divisors = intersect(z_4_divisors,z_3_divisors);
 
 if ~isempty(stage2_divisors)
-    disp(["gcd stage 2: ",gcd(z_3,z_4)]);
+    disp(["stage 2 divisors:",stage2_divisors]);
     warning("stage 2 gears are not relative prime")
+else
+    disp(["stage 2 divisors:",stage2_divisors]);
+    % disp("\nStage 1 gears ARE relative")
 end
 
+final_i_tot = (z_2/z_1_min) * (z_4/z_3)
 
 function d = alldivisors(n)
     % taken from: https://www.mathworks.com/matlabcentral/answers/21542-find-divisors-for-a-given-number#answer_28371
