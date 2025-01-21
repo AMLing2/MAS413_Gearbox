296538dcce1f8fa906f702c3b69eec2f3ba4cf82
AMLing2 <mathias.lervik@hotmail.com>
2025-01-21 14:23:07 +0100

    added different divisor method

--- Scripts/s2_stage.m
+++ Scripts/s2_stage.m
@@ -8,14 +8,38 @@ beta = 15; % helix angle [deg]
 n_out = n_1/i_tot;
 
 
-%calculations for internal gears
-i_1 = 4.5; % first gear ratio of 2 stage from table 15-38, Lec2 pg12
-i_2 = 17.3/i_1
+% calculations for internal gears
+i_1 = 5; % first gear ratio of 2 stage from table 15-38, Lec2 pg12
+i_2 = i_tot/i_1;
 
-z_1_min = 18; % minimum 18-20 from teacher
-z_2 = i_1 * z_1_min;
+z_1_min = 19; % minimum 18-20 from teacher [# teeth]
+z_2 = i_1 * z_1_min
 
-if gcd(z_1_min,z_2) > 1
-    gcd(z_1_min,z_2)
+z_1_divisors = alldivisors(z_1_min);
+z_2_divisors = alldivisors(z_2);
+stage1_divisors = intersect(z_1_divisors,z_2_divisors);
+
+if ~isempty(stage1_divisors) % greatest common denominator
+    disp(["gcd stage 2: ",gcd(z_1_min,z_2)]);
     warning("stage 1 gears are not relative prime")
+end
+
+z_4 = 173; % from converting i_2 to rational
+z_3 = 50;
+
+z_4_divisors = alldivisors(z_4);
+z_3_divisors = alldivisors(z_3);
+stage2_divisors = intersect(z_4_divisors,z_3_divisors);
+
+if ~isempty(stage2_divisors)
+    disp(["gcd stage 2: ",gcd(z_3,z_4)]);
+    warning("stage 2 gears are not relative prime")
+end
+
+
+function d = alldivisors(n)
+    % taken from: https://www.mathworks.com/matlabcentral/answers/21542-find-divisors-for-a-given-number#answer_28371
+    K = 1:n;
+    d = K(rem(n,K)==0);
+    d(1) = []; % remove first element of 1
 end
