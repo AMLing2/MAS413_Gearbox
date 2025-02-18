33463f6a6b40fb9b488262af92305bfabd944f69
Oletn <142843101+Oletn@users.noreply.github.com>
2025-02-18 13:04:35 +0100

    Update mechOfMaterials_shaft2.m

--- Scripts/mechOfMaterials_shaft2.m
+++ Scripts/mechOfMaterials_shaft2.m
@@ -2,52 +2,66 @@
  % MAS413 Project: Mechanics of Materials - Shaft 2
  
  %% Constants
- 
- % Given information from project description - Needs adjustment of value
- n_1 = 1450; % [RPM]
- omega_1 = n_1 * 2*pi / 60; % [rad/sec]
- P_1 = 12.5e3; % [W]
+ % Given information from project description - Needs adjustment of values
  i_tot = 17.3; % [-]
+ 
+ n_in = 1450; % [RPM]
+ omega_in = n_in * 2*pi / 60; % [rad/sec]
+ 
+ n_out = (n_in/i_tot); %[RPM]
+ omega_out = n_out * 2*pi / 60; %[rad/sec]
+
+ 
+ P_in = 12.5e3; % [W]
+ eta = 0.96; %[-] Efficiency
+ eta_tot = eta^2;
+ P_out = P_in*eta_tot; %[W] (Squared efficiency because there are two stages)
+    
+ T_M = P_in/omega_in; %[Nm]
+ T_out = P_out/omega_out; %[Nm] 
+
  i_1 = 1; % [-]
  i_2 = 2; % [-]
  alpha = 20; % [degrees] Helix Angle
  beta = 15;  % [degrees] Pressure Angle
- 
-
 
  % Lengths on shaft - Needs adjustment of value
- L_E_G3  = 0.05; % [m]
- L_G3_G2 = 0.10; % [m]
- L_G2_D = 0.15; % [m]
- L_tot = L_E_G3 + L_G3_G2 + L_G2_D; % [m]
+ L_EG3  = 0.05; % [m]
+ L_G3G2 = 0.10; % [m]
+ L_G2D = 0.15; % [m]
+ L_tot = L_EG3 + L_G3G2 + L_G2D; % [m]
+ L_G3D = L_G3G2+L_G2D; % [m]
  
  % Radius of gears - Calculated Elsewhere - Needs adjustment of value
- r_G2 = 0.25  % [m]
+ r_G1 = 0.25; % [m]
+ r_G2 = 0.25; % [m]
  r_G3 = 0.25; % [m]
- 
- 
+ r_G4 = 0.25; % [m]
+
+ %% Calculations
  % Calculated lengths & Torques
- L_E_G2 = L_E_G3 + L_G3_G2; % [m]
- 
- T_M = P_1 / omega_1; % [Nm]
- T_S2 = T_M * i1; %[Nm]
+ L_E_G2 = L_EG3 + L_G3G2; % [m]
  
  %Gear 2 forces
- F_t2 = T_S2/r_G2; % [N]
+ F_t2 = T_M/r_G1; % [N]
  F_a2 = F_t2 * tand(beta); % [N]
  F_r2 = F_t2 * tand(alpha)/cosd(beta); % [N]
  
  % Gear 3 forces
- F_t3 = T_S2 / r_G3; % [N]
+ 
+ F_t3 = (T_out*i_tot) / r_G4; % [N]
  F_a3 = F_t3 * tand(beta); % [N]
  F_r3 = F_t3 * tand(alpha)/cosd(beta); % [N]
  
- % Reaction forces @ bearings
- L_BC = L_BG1 + L_G1C; % [m]
+ %% Reaction forces @ bearings
+ L_ED = L_tot;
 
- F_By = F_t1*L_G1C/L_BC; % [N]
- F_Bz = F_r1*L_G1C/L_BC; % [N]
- 
+ F_Ex = F_a2-F_a3;
+
+ F_Ez = (F_r3*L_G3D - F_r2*L_G2D - F_a3*r_G3 - F_a2*r_G2)/L_ED;
+
+ F_Ey= (F_t3*L_G3D + F_t2*L_G2D)/L_ED;
+
  
  %% XY - Plane
  
@@ -82,11 +96,11 @@
  
  % 0 < x < L_E_G3
  
- x = linspace(0, L_AB, resolution);
- %P = zeros(size(x)); % [N]
- %V = zeros(size(x)); % [N]
- %M = zeros(size(x)); % [Nm]
- %T = ones(size(x)) * T_S2; % [Nm]
+  x = linspace(0, L_EG3, resolution);
+  P = ones(size(x)) * F_Ex; % [N]
+  V = ones(size(x)) * (-F_Ey); % [N]
+  M = ones(size(x)) .* (-F_Ey * x); % [Nm]
+  T = zeros(size(x)); % [Nm]
  
  subplot(2,2,1)
  hold on
@@ -103,11 +117,11 @@
  
  % L_E_G3 < x < L_G3_G2
  
- x = linspace(L_E_G3, L_G3_G2, resolution);
- %P = zeros(size(x)); % [N]
- %V = ones(size(x)) * (-F_By); % [N]
- %M = F_By * (x - L_AB); % [Nm]
- %T = ones(size(x)) * T_S2; % [Nm]
+ x = linspace(L_EG3, L_G3G2, resolution);
+ P = ones(size(x))* -(F_Ex+F_a3); % [N]
+ V = ones(size(x)) * -(F_Ey+F_t3); % [N]
+ M = -F_Ey*x + F_t3*(x - L_EG3); % [Nm]
+ T = ones(size(x)) * F_t3*r_G3; % [Nm]
  
  subplot(2,2,1)
  hold on
@@ -124,115 +138,114 @@
  
  % L_G3_G2 < x < L_G2_D
  
-% x = linspace(L_G3_G2, L_AC, resolution);
-% P = ones(size(x)) * (-F_a1); % [N]
-% V = ones(size(x)) * (F_By - F_t1); % [N]
-% M = F_By * (x - L_AB) - F_t1 * (x - L_AG1); % [Nm]
-% T = ones(size(x)) * (T_S2 - F_t1*r_G3); % [Nm]
-% 
-% subplot(2,2,1)
-% hold on
-% plot(x,P,'r','LineWidth',2)
-% subplot(2,2,2)
-% hold on
-% plot(x,V,'r','LineWidth',2)
-% subplot(2,2,3)
-% hold on
-% plot(x,M,'Color','#FF8800','LineWidth',2)
-% subplot(2,2,4)
-% hold on
-% plot(x,T,'Color','#FF8800','LineWidth',2)
-% 
+ x = linspace(L_G3G2, L_G2D, resolution);
+ P = ones(size(x)) * (F_Ex - F_a2 - F_a3); % [N]
+ V = ones(size(x)) * (F_t2 - F_Ey + F_t3); % [N]
+ M = F_t2 * (x- L_E_G2) - F_Ey*x + F_t3 * (x - L_EG3); % [Nm]
+ %T = ones(size(x)) * (F_t3*r_G3 - F_t2*r_G2); % [Nm]
+ 
+ subplot(2,2,1)
+ hold on
+ plot(x,P,'r','LineWidth',2)
+ subplot(2,2,2)
+ hold on
+ plot(x,V,'r','LineWidth',2)
+ subplot(2,2,3)
+ hold on
+ plot(x,M,'Color','#FF8800','LineWidth',2)
+ subplot(2,2,4)
+ hold on
+ plot(x,T,'Color','#FF8800','LineWidth',2)
+ 
 % %% XZ - Plane
-% 
-% % Figure setup
-% resolution = 100;
-% figHandle = 2;
-% xPos = 10;
-% yPos = 3;
-% wPlot = 22;
-% hPlot = 16;
-% 
-% XZplaneFig = figure(figHandle);
-% set(figHandle,'Units','Centimeter')
-% set(figHandle,'Position',[xPos yPos wPlot hPlot]);
-% sgtitle('XZ - Plane')
-% subplot(2,2,1)
-% xlabel('[m]')
-% ylabel('[N]')
-% title('Axial Force P(x)')
-% subplot(2,2,2)
-% xlabel('[m]')
-% ylabel('[N]')
-% title('Shear Force V(x)')
-% subplot(2,2,3)
-% xlabel('[m]')
-% ylabel('[Nm]')
-% title('Bending Moment M(x)')
-% subplot(2,2,4)
-% xlabel('[m]')
-% ylabel('[Nm]')
-% title('Axial Torque T(x)')
-% 
-% % 0 < x < L_E_G3
-% 
-% x = linspace(0, L_E_G3, resolution);
-% P = zeros(size(x)); % [N]
-% V = zeros(size(x)); % [N]
-% M = zeros(size(x)); % [Nm]
-% T = ones(size(x)) * T_S2; % [Nm]
-% 
-% subplot(2,2,1)
-% hold on
-% plot(x,P,'r','LineWidth',2)
-% subplot(2,2,2)
-% hold on
-% plot(x,V,'r','LineWidth',2)
-% subplot(2,2,3)
-% hold on
-% plot(x,M,'Color','#FF8800','LineWidth',2)
-% subplot(2,2,4)
-% hold on
-% plot(x,T,'Color','#FF8800','LineWidth',2)
-% 
-% % L_E_G3 < x < L_G3_G2
-% 
-% x = linspace(L_E_G3, L_G3_G2, resolution);
-% P = zeros(size(x)); % [N]
-% V = ones(size(x)) * (F_Bz); % [N]
-% M = - F_Bz * (x - L_AB); % [Nm]
-% T = ones(size(x)) * T_S2; % [Nm]
-% 
-% subplot(2,2,1)
-% hold on
-% plot(x,P,'r','LineWidth',2)
-% subplot(2,2,2)
-% hold on
-% plot(x,V,'r','LineWidth',2)
-% subplot(2,2,3)
-% hold on
-% plot(x,M,'Color','#FF8800','LineWidth',2)
-% subplot(2,2,4)
-% hold on
-% plot(x,T,'Color','#FF8800','LineWidth',2)
-% 
-% % L_G3_G2 < x < L_G2_D
-% 
-% x = linspace(L_G3_G2, L_G2_D, resolution);
-% P = ones(size(x)) * (-F_a1); % [N]
-% V = ones(size(x)) * (F_Bz - F_r1); % [N]
-% M = F_r1 * (x - L_AG1) - F_Bz * (x - L_AB) - F_a1 * r_G3; % [Nm]
-% T = ones(size(x)) * (T_S2 - F_t1*r_G3); % [Nm]
-% 
-% subplot(2,2,1)
-% hold on
-% plot(x,P,'r','LineWidth',2)
-% subplot(2,2,2)
-% hold on
-% plot(x,V,'r','LineWidth',2)
-% subplot(2,2,3)
-% hold on
-% plot(x,M,'Color','#FF8800','LineWidth',2)
-% subplot(2,2,4)
-% hold on
-% plot(x,T,'Color','#FF8800','LineWidth',2)
+ 
+ % Figure setup
+ resolution = 100;
+ figHandle = 2;
+ xPos = 10;
+ yPos = 3;
+ wPlot = 22;
+ hPlot = 16;
+ 
+ XZplaneFig = figure(figHandle);
+ set(figHandle,'Units','Centimeter')
+ set(figHandle,'Position',[xPos yPos wPlot hPlot]);
+ sgtitle('XZ - Plane')
+ subplot(2,2,1)
+ xlabel('[m]')
+ ylabel('[N]')
+ title('Axial Force P(x)')
+ subplot(2,2,2)
+ xlabel('[m]')
+ ylabel('[N]')
+ title('Shear Force V(x)')
+ subplot(2,2,3)
+ xlabel('[m]')
+ ylabel('[Nm]')
+ title('Bending Moment M(x)')
+ subplot(2,2,4)
+ xlabel('[m]')
+ ylabel('[Nm]')
+ title('Axial Torque T(x)')
+ 
+ % 0 < x < L_E_G3
+ 
+ x = linspace(0, L_EG3, resolution);
+ P = ones(size(x))*(-F_Ex); % [N]
+ V = ones(size(x))*(-F_Ez); % [N]
+ M = ones(size(x)).* (x *(-F_Ex)); % [Nm]
+ T = zeros(size(x)); % [Nm]
+ 
+ subplot(2,2,1)
+ hold on
+ plot(x,P,'r','LineWidth',2)
+ subplot(2,2,2)
+ hold on
+ plot(x,V,'r','LineWidth',2)
+ subplot(2,2,3)
+ hold on
+ plot(x,M,'Color','#FF8800','LineWidth',2)
+ subplot(2,2,4)
+ hold on
+ plot(x,T,'Color','#FF8800','LineWidth',2)
+ 
+ % L_E_G3 < x < L_G3_G2
+ x = linspace(L_EG3, L_G3G2, resolution);
+ P = ones(size(x))* -(F_Ex+F_a3); % [N]
+ V = ones(size(x)) * (F_r3-F_Ez); % [N]
+ M = F_Ez*x - F_r3*(x - L_EG3); % [Nm]
+ T = ones(size(x)) * F_t3*r_G3; % [Nm]
+ 
+ subplot(2,2,1)
+ hold on
+ plot(x,P,'r','LineWidth',2)
+ subplot(2,2,2)
+ hold on
+ plot(x,V,'r','LineWidth',2)
+ subplot(2,2,3)
+ hold on
+ plot(x,M,'Color','#FF8800','LineWidth',2)
+ subplot(2,2,4)
+ hold on
+ plot(x,T,'Color','#FF8800','LineWidth',2)
+ 
+ % L_G3_G2 < x < L_G2_D
+ 
+ x = linspace(L_G3G2, L_G2D, resolution);
+ P = ones(size(x)) * (F_a2 - F_a3 - F_Ex); % [N]
+ V = ones(size(x)) * (F_r3 - F_Ez - F_r2); % [N]
+ M = F_Ez * x - F_r3*(x - L_EG3) + F_a3 * r_G3 * (x - L_EG3) + F_a2*r_G2*(x-L_G3G2); % [Nm]
+ T = ones(size(x)) * (F_t3*r_G3 - F_t2*r_G2); % [Nm]
+ 
+ subplot(2,2,1)
+ hold on
+ plot(x,P,'r','LineWidth',2)
+ subplot(2,2,2)
+ hold on
+ plot(x,V,'r','LineWidth',2)
+ subplot(2,2,3)
+ hold on
+ plot(x,M,'Color','#FF8800','LineWidth',2)
+ subplot(2,2,4)
+ hold on
+ plot(x,T,'Color','#FF8800','LineWidth',2)
