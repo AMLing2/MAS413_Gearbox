6cbaa3382988726689a270f2d33078aa0d0c3374
Thomas Lønne Stiansen <thomal2002@hotmail.com>
2025-03-14 08:40:49 +0100

    review TLS shaft 2 - Changed sign in eq. (3.24a) F_a2-F_a3-F_Ex (axial forces in 3rd interval) also added spacing in 2 other places

--- Scripts/loadingDiagrams_shaft2.m
+++ Scripts/loadingDiagrams_shaft2.m
@@ -118,9 +118,9 @@ xy_T = [xy_T,ones(size(x)) * F_t3*r_G3]; % [Nm]
 % L_EG2 < x < L_ED
 x = linspace(L_EG2, L_ED, resolution);
 xy_x = [xy_x, x];
-xy_P = [xy_P, ones(size(x)) * (-F_Ex - F_a2 + F_a3)]; % [N]
+xy_P = [xy_P, ones(size(x)) * (-F_Ex - F_a3 + F_a2)]; % [N]
 xy_V = [xy_V, ones(size(x)) * (F_t2 - F_Ey + F_t3)]; % [N]
-xy_M = [xy_M, ( F_t2*(x- L_EG2) - F_Ey*(x) + F_t3*(x - L_EG3) )]; % [Nm]
+xy_M = [xy_M, ( F_t2*(x - L_EG2) - F_Ey*(x) + F_t3*(x - L_EG3) )]; % [Nm]
 xy_T = [xy_T, ones(size(x)) * (F_t3*r_G3 - F_t2*r_G2)]; % [Nm]
  
 subplot(2,2,1)
@@ -179,7 +179,7 @@ xz_T = [xz_T,zeros(size(x))]; % [Nm]
 % L_EG3 < x < L_EG2
 x = linspace(L_EG3, L_EG2, resolution);
 xz_x = [xz_x, x];
-xz_P = [xz_P,ones(size(x))* -(F_Ex + F_a3)]; % [N]
+xz_P = [xz_P,ones(size(x))* ( - F_Ex - F_a3)]; % [N]
 xz_V = [xz_V,ones(size(x)) * (F_r3 - F_Ez)]; % [N]
 xz_M = [xz_M, ( -F_Ez*(x) + F_r3*(x - L_EG3) - F_a3*(r_G3) )]; % [Nm]
 xz_T = [xz_T,ones(size(x)) * F_t3*r_G3]; % [Nm]
