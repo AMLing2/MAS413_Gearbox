a6c602d44c74e6d7ef9247f7eb5bb2f468862f80
Thomas Lønne Stiansen <thomal2002@hotmail.com>
2025-03-15 16:03:31 +0100

    Readability & notation consistency in grat2stage()

--- Scripts/grat2stage.m
+++ Scripts/grat2stage.m
@@ -10,19 +10,19 @@ function [z_smallest,z_closest] = grat2stage(i_s1_min,i_s1_max,inc,z_1,i_tot)
   z_smallest = inf(1,5);
   z_closest = zeros(1,5);
   z_closest(5) = inf;
-  s2_z1_min = 18; % same min as z_1 min
-  s2_z1_max = 30; % arbitrary max, expensive to go over this, both min and max subject to change
-  tol = 0.001; % only used for printing
-  for s1_grat = i_s1_min : inc : i_s1_max
-    z_2 = round(s1_grat * z_1);
+  s2_z3_min = 18; % same min as z_1 min
+  s2_z3_max = 30; % arbitrary max, expensive to go over this, both min and max subject to change
+  % tol = 0.001; % only used for printing
+  for i_s1 = i_s1_min : inc : i_s1_max
+    z_2 = round(i_s1 * z_1);
     while gcd(z_2,z_1) > 1 % -1 to z2 until its relative prime (-1 gave closer final results)
       z_2 = z_2 - 1;
     end
-    s2_grat = i_tot/s1_grat;
-    for z_3 = s2_z1_min : s2_z1_max
-      z_4 = round(s2_grat * z_3);
+    i_s2 = i_tot / (z_2/z_1);
+    for z_3 = s2_z3_min : s2_z3_max
+      z_4 = round(i_s2 * z_3);
       while gcd(z_4,z_3) > 1 % same as above, +1 gave closer results here
-        z_4 = z_4 + 1;
+        z_4 = z_4 - 1;
       end
       i_tot_t = (z_2/z_1) * (z_4/z_3); % calc total gear ratio
 
@@ -40,9 +40,9 @@ function [z_smallest,z_closest] = grat2stage(i_s1_min,i_s1_max,inc,z_1,i_tot)
         z_closest(1) = z_1; z_closest(2) = z_2; % save to array
         z_closest(3) = z_3;  z_closest(4) = z_4;
         z_closest(5) = i_tot_t;
-        if ismembertol(z_closest(5),i_tot,tol) % i_tot_o == i_tot
-          sprintf("close match found: \n z_1: %d , z_2: %d \n z_3: %d , z_4: %d \n i_tot: %.6f ",z_1,z_2, z_3, z_4, i_tot_t)
-        end
+        % if ismembertol(z_closest(5), i_tot, tol) % i_tot_o == i_tot
+        %   sprintf("close match found: \n z_1: %d , z_2: %d \n z_3: %d , z_4: %d \n i_tot: %.6f ",z_1,z_2, z_3, z_4, i_tot_t)
+        % end
       end
     end
   end
