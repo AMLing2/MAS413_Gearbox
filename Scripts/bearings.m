fcce0319b033f27fbb9651ae076b279e4d1c85e9
Thomas Lønne Stiansen <thomal2002@hotmail.com>
2025-03-22 10:14:48 +0100

    Cleaned diagrams folder content and uploaded 20 ball bearing data from SKF catalogue

--- Scripts/bearings.m
+++ Scripts/bearings.m
@@ -10,27 +10,41 @@ work_cycle = 10; % [hours/day]
 load('gear_sizes.mat', 'z_1','z_2','z_3','z_4','i_tot','i_s1','i_s2', ...
     'n_1','n_2','n_3','n_4')
 
+% Import from Loading Diagrams
+load('loadingDiagram_shaft1.mat', 'xz_P', 'xy_V', 'xz_V')
+shaft1_Fa = xz_P;
+shaft1_Fr = sqrt(xy_V^2 + xz_V^2);
+load('loadingDiagram_shaft2.mat', 'xz_P', 'xy_V', 'xz_V')
+shaft2_Fa = xz_P;
+shaft2_Fr = sqrt(xy_V^2 + xz_V^2);
+load('loadingDiagram_shaft3.mat', 'xz_P', 'xy_V', 'xz_V')
+shaft3_Fa = xz_P;
+shaft3_Fr = sqrt(xy_V^2 + xz_V^2);
+
+% Import from Shaft Design
+load('shaftDesign.mat', 'd_S11', 'd_C')
+
 % number of cycles through lifetime:
 cycles_lifetime_sh1 = lifetime * 365.25 * work_cycle*60 * n_1
 cycles_lifetime_sh2 = lifetime * 365.25 * work_cycle*60 * n_2
 cycles_lifetime_sh3 = lifetime * 365.25 * work_cycle*60 * n_4
 
-% Loads from loadingDiagrams_shaftx.m scripts:
-% - radial [N]:
-F_r_sh1 = 20; % sqrt(F_Cy^2 + F_Cz^2)
-F_r_sh2 = 20; % max(sqrt(F_Ey^2 + F_Ez^2), sqrt(F_Dy^2 + F_Dz^2))
-F_r_sh3 = 20; % sqrt(F_Fy^2 + F_Fz^2)
-% - axial  [N]:
-F_a_sh1 = 5; % F_Cx
-F_a_sh2 = 5; % F_Ex
-F_a_sh3 = 5; % F_Fx , check if negative
+% % Loads from loadingDiagrams_shaftx.m scripts:
+% % - radial [N]:
+% F_r_sh1 = 20; % sqrt(F_Cy^2 + F_Cz^2)
+% F_r_sh2 = 20; % max(sqrt(F_Ey^2 + F_Ez^2), sqrt(F_Dy^2 + F_Dz^2))
+% F_r_sh3 = 20; % sqrt(F_Fy^2 + F_Fz^2)
+% % - axial  [N]:
+% F_a_sh1 = 5; % F_Cx
+% F_a_sh2 = 5; % F_Ex
+% F_a_sh3 = 5; % F_Fx , check if negative
 
-% minimum shaft diameters from shaftDesign.m [mm]
-d_min_sh1 = 0;
-d_min_sh1_m = 0; % motor / input side of shaft 1
-d_min_sh2 = 0;
-d_min_sh3 = 0;
-d_min_sh3_p = 0; % output side of shaft 3
+% % minimum shaft diameters from shaftDesign.m [mm]
+% d_min_sh1 = 0;
+% d_min_sh1_m = 0; % motor / input side of shaft 1
+% d_min_sh2 = 0;
+% d_min_sh3 = 0;
+% d_min_sh3_p = 0; % output side of shaft 3
 
 K_R = 0.62; % Reliability factor for weibull distribution 95% R%, tab 11-5 pg 701 machine design
 
@@ -46,8 +60,8 @@ taper_ang = taper_ang_list(ang_index);
 
 d_list = 50:10:150; % [mm]
 bearing_name = ["32912" "32914" "32916" "32918" "32920" "32922" "32924" "32928" "32930"];
-c_dyn_list = [49.5 76.8 80.6 105 136 143 185 222 228 305] * 1e3;% [kN] -> [N]
-c_st_list = [82.3 123 136 178 231 253 326 428 545] * 1e3; % [kN] -> [N]
+c_dyn_list = [49.5 76.8 80.6 105 136 143 185 222 228 305] * 1e3; % [N]
+c_st_list = [82.3 123 136 178 231 253 326 428 545] * 1e3; % [N]
 
 [b_index_1,n1] = tapered_bearing_sizing(d_min_sh1 ,F_r_sh1,F_a_sh1,cycles_lifetime_sh1, ...
     taper_ang, K_R, d_list, c_dyn_list, c_st_list)
@@ -56,7 +70,7 @@ c_st_list = [82.3 123 136 178 231 253 326 428 545] * 1e3; % [kN] -> [N]
 [b_index_3,n3] = tapered_bearing_sizing(d_min_sh3 ,F_r_sh1,F_a_sh3,cycles_lifetime_sh3, ...
     taper_ang, K_R, d_list, c_dyn_list, c_st_list);
 
-%% deep groove ball bearing selection
+% Deep Groove Ball Bearing Selection
 % need to update c_dyn_list and c_st_list with vales from:
 % https://www.skf.com/group/products/rolling-bearings/ball-bearings/deep-groove-ball-bearings#cid-493604
 d_list_ball = 50:5:150; % [mm]
@@ -64,6 +78,9 @@ d_list_ball = 50:5:150; % [mm]
     K_R, d_list_ball, c_dyn_list, c_st_list); 
 [b_index_p,n_bp] = ball_bearing_sizing(d_min_sh3_p ,F_r_sh3,0,cycles_lifetime_sh3, ...
     K_R, d_list_ball, c_dyn_list, c_st_list); 
+
+
+%% Tapered Roller Bearing Selection
 
 function [bearing_index,lifetime] = tapered_bearing_sizing(d_min,F_r,F_a,cycles,taper_ang,K_R,d_list,C_dyn_list,C_st_list)
 % single row tapered roller bearing
@@ -125,6 +142,8 @@ function [bearing_index,lifetime] = tapered_bearing_sizing(d_min,F_r,F_a,cycles,
     end
 end
 
+
+%% Deep Groove Ball Bearing Selection
 function [bearing_index,lifetime] = ball_bearing_sizing(d_min,F_r,F_a,cycles,K_R,d_list,C_dyn_list,C_st_list)
 % single row deep groove/conrad ball bearing
 
