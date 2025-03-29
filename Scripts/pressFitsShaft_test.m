ebf4b17e5c84e5c32646ae2d3f80f775520638dc
AMLing2 <mathias.lervik@hotmail.com>
2025-03-29 17:04:50 +0100

    bearing shrink fit calcs

--- Scripts/pressFitsShaft_test.m
+++ Scripts/pressFitsShaft_test.m
@@ -6,16 +6,16 @@ mu = 0.17; % static dry, mild steel on mild steel, tab 7-1 pg 464 machine design
 E_mat = 200; % [GPa]
 V_mat = 0.29;
 [p,T_max,d_c,h,s,heat_temp_hub,cool_temp_shaft,sigma_t_s,sigma_t_o,sigma_r_s,sigma_r_o] = ...
-    pressFitsShaft(d_hub_outer,d_shaft,l_hub,mu,E_mat,E_mat,V_mat,V_mat);
+    shrinkFitGear(d_hub_outer,d_shaft,l_hub,mu,E_mat,E_mat,V_mat,V_mat);
 
 bearing_inner = 70; % [mm]
-[p1,T_max1,d_c1,h1,s1,heat_temp1,cool_temp1,sigma_t_s1,sigma_t_o1,sigma_r_s1,sigma_r_o1] = ...
-    pressFitsHub(d_hub_outer,d_shaft,l_hub,mu,E_mat,E_mat,V_mat,V_mat,bearing_inner);
+bearing_outer = 100; % [mm]
+[shaft_d_new1,h1,s1,heat_temp_hub1,cool_temp_shaft1,bearing_clearance] = shrinkFitBearing(bearing_outer,bearing_inner);
 
 datatable = table([p,T_max,d_c,h,s,heat_temp_hub,cool_temp_shaft,sigma_t_s,sigma_t_o,sigma_r_s,sigma_r_o]' ...
     , ["p","T_max","d_c","h","s","heat_temp","cool_temp_shaft","sigma_t_s","sigma_t_o","sigma_r_s","sigma_r_o"]', 'VariableNames', {'data', 'name'});
 disp(datatable);
-return
-datatable1 = table([p1,T_max1,d_c1,h1,s1,heat_temp1,cool_temp1,sigma_t_s1,sigma_t_o1,sigma_r_s1,sigma_r_o1]' ...
-    , ["p1","T_max1","d_c1","h1","s1","heat_temp1","cool_temp1","sigma_t_s1","sigma_t_o1","sigma_r_s1","sigma_r_o1"]', 'VariableNames', {'data', 'name'});
+
+datatable1 = table([shaft_d_new1,h1,s1,heat_temp_hub1,cool_temp_shaft1,bearing_clearance]' ...
+    , ["shaft_d_new1","h1","s1","heat_temp_hub1","cool_temp_shaft1","bearing_clearance"]', 'VariableNames', {'data', 'name'});
 disp(datatable1);
---
+++ Scripts/shrinkFitBearing.m
@@ -0,0 +1,78 @@
+% chapter 10.12 machine design pg 620
+% case: known hub inner diameter (bearing selected), increase shaft outer diamter to fit the hub
+% (for bearings)
+%%%INPUTS:
+% d_h_o : outer diameter of hub / gear [mm]
+% d_s : inner diameter of hub [mm]
+% l : length of hub/gear engagement [mm]
+% mu : coefficient of friction, static
+% E_o : Young's modulus of gear / hub material [GPa]
+% E_i : Young's modulus of shaft material [GPa]
+% V_o : poisson's ratio of gear / hub material
+% V_i : poisson's ratio of shaft material
+% d_h_i : inner diameter of hub / bearing [mm]
+
+%%%OUTPUTS:
+% p : pressure in the interference [Mpa]
+% T_max : maximum torque the joint can withstand [Nm]
+% d_c : changed radius of outer shaft radius [mm]
+% h : gear hole diameter tolerence
+% s : shaft diameter tolerence
+% heat_temp_hub : temperature of hub / bearing needed for the fit [deg C]
+% temp_shaft : temperature of shaft needed for the fit [deg C]
+% sigma_t_s = tangenial stress in shaft [Mpa]
+% sigma_t_o = radial stress in shaft [Mpa]
+% sigma_r_s = tangenial stress in hub / bearing [Mpa]
+% sigma_r_o = radial stress in hub / bearing [Mpa]
+
+function [d_s_c,h,s,heat_temp_bearing,temp_shaft,bearing_clearance] ...
+    = shrinkFitBearing(d_h_o,d_h_i)
+
+    % class 8 fit (h8), Appendix E-1 fundamentals of machine component design pg 854
+    C_h = 0.0052;
+    C_s = 0.0052;
+    C_i_8 = 0.0010;
+    C_i_7 = 0.0005;
+    % find values for a k5 fit
+    i_7 = C_i_7 * d_h_i; % [mm] average interference h7
+    i_8 = C_i_8 * d_h_i; % [mm] average interference h8
+    
+    d_s_c = d_h_i + i_8; % [mm] new radius of shaft
+    h = C_h * d_h_i^(1/3); % gear hole diameter tolerence 
+    s = C_s * d_s_c^(1/3); % shaft diameter tolerence 
+
+    % temperature calculations: pg 577 university physics book
+    temp_room = 22; % room temperature [deg C]
+    temp_shaft = temp_room;
+    L_0 = 2*pi*(d_h_i/2); % initial circumference [mm]
+    fit_tol = s+h; % extra radius for easier fit [mm]
+    L_heated = 2*pi*((d_h_i + i_8 + fit_tol)/2); % finishing length for fit [mm]
+    alpha = 1.2e-5; % Coefficient of liear expansion [1/deg C], tab 17.1 pg 578
+    heat_temp_bearing = ((L_heated - L_0) / (alpha * L_0)) + temp_room; % [deg C] eq 17.6 pg 576
+
+    max_heating = 110; % bad for bearings to be above 125 deg C
+    if heat_temp_bearing > max_heating
+        %warning("bearing d = %dmm has to be cooled for interference fit",d_s)
+        temp_shaft = temp_room - (heat_temp_bearing - max_heating); % cool down shaft instead
+        heat_temp_bearing = max_heating;
+    end
+
+    % bearing clearance reduction calcs
+    temp_outer = 68; % [deg C] temperature of outer ring, estimate
+    working_temp = 70; % [deg C]
+    d_m = (d_h_o + d_h_i) / 2; % [mm] bearing mean diameter
+    delta_r_temp = 0.012 * (working_temp - temp_outer) * d_m; % [um], converts to [um] in the datasheet
+    f1 = 0.4773 * (d_h_i/d_h_o) + 0.5507; % reduction factor for inner ring, diagram 2 pg184 skf catalog assuming linear func
+    delta_r_fit = f1 * i_8 * 1e3; % [um] assuming loose fit on outer ring seat
+    r_op = 0; % [um] required operating clearance, equal to 0 for ball bearings
+    r = r_op + delta_r_temp + delta_r_fit; % pg 184 skf datasheet
+    
+    clearance_list = [2 2 3 5 5 6 8 10 12 15 18 18 20 25]; % pg 252 table 6 skf catalog
+    bore_diam_list = [2.5 6 10 18 24 30 40 50 65 80 100 120 140 160 180 200];
+    [~,i] = closest(bore_diam_list,d_h_i);
+    min_clearance = clearance_list(i);
+    bearing_clearance = min_clearance - r; % [um]
+    if bearing_clearance < 0
+        warning("bad bearing clearance")
+    end
+end
