66507dc7e49b8e04c835c66c77bfe46e3cf99862
Thomas Lønne Stiansen <thomal2002@hotmail.com>
2025-04-04 13:16:47 +0200

    Update oil to english and comments

--- Scripts/oil_selection.m
+++ Scripts/oil_selection.m
@@ -9,7 +9,8 @@ end
 
 if exist(fullfile(export_import,'gear_sizes.mat'), 'file')
     load(fullfile(export_import,'gear_sizes.mat'), ...
-    "b_s1", "b_s2", "d_g3", "d_g4", "d_g1", "d_g2", "v_p_g1","v_p_g2", "v_p_g3", "v_p_g4",...
+    "b_s1", "b_s2", "d_g3", "d_g4", "d_g1", "d_g2", ...
+    "v_p_g1","v_p_g2", "v_p_g3", "v_p_g4",...
     "n_1", "n_2", "n_3","n_4")
 end
 
@@ -19,29 +20,24 @@ if exist(fullfile(export_import,'bearings.mat'), 'file')
 end
 
 %% Gear oil
-%Gear one
-Belastning_hastighetsfaktorer_g1 = ks(F_t1, b_s1, d_g1,i_s1) / v_p_g1;%[ (N*s) /  (mm^2*m) ]
 
-%Gear two
-Belastning_hastighetsfaktorer_g2 = ks(F_t2, b_s1, d_g2,i_s1) / v_p_g2;%[ (N*s) /  (mm^2*m) ]
-
-%Gear three
-Belastning_hastighetsfaktorer_g3 = ks(F_t3, b_s2, d_g3,i_s2) / v_p_g3;%[ (N*s) /  (mm^2*m) ]
-
-%Gear four
-Belastning_hastighetsfaktorer_g4 = ks(F_t4, b_s2, d_g4,i_s2) / v_p_g4;%[ (N*s) /  (mm^2*m) ]
-
-
+% Gear one
+loadSpeed_factor_g1 = ks(F_t1, b_s1, d_g1,i_s1) / v_p_g1; % [N·s]/[mm²·m]
+% Gear two
+loadSpeed_factor_g2 = ks(F_t2, b_s1, d_g2,i_s1) / v_p_g2; % [N·s]/[mm²·m]
+% Gear three
+loadSpeed_factor_g3 = ks(F_t3, b_s2, d_g3,i_s2) / v_p_g3; % [N·s]/[mm²·m]
+% Gear four
+loadSpeed_factor_g4 = ks(F_t4, b_s2, d_g4,i_s2) / v_p_g4; % [N·s]/[mm²·m]
 
 format compact
-fprintf('Load and speed factor for gear 1: %.1f [N·s]/[mm²·m]\n', Belastning_hastighetsfaktorer_g1);
-fprintf('Load and speed factor for gear 2: %.1f [N·s]/[mm²·m]\n', Belastning_hastighetsfaktorer_g2);
-fprintf('Load and speed factor for gear 3: %.1f [N·s]/[mm²·m]\n', Belastning_hastighetsfaktorer_g3);
-fprintf('Load and speed factor for gear 4: %.1f [N·s]/[mm²·m]\n', Belastning_hastighetsfaktorer_g4);
+fprintf('Load and speed factor for gear 1: %.1f [N·s]/[mm²·m]\n', loadSpeed_factor_g1);
+fprintf('Load and speed factor for gear 2: %.1f [N·s]/[mm²·m]\n', loadSpeed_factor_g2);
+fprintf('Load and speed factor for gear 3: %.1f [N·s]/[mm²·m]\n', loadSpeed_factor_g3);
+fprintf('Load and speed factor for gear 4: %.1f [N·s]/[mm²·m]\n', loadSpeed_factor_g4);
 fprintf('\n');
 
 %% Bearing Oil
-
 
 % Bearing B
 fprintf('Mean diameter for bearing B: %.1f [mm]\n', dm_B);
@@ -64,46 +60,27 @@ fprintf('RPM of shaft 2: %.1f [rpm]\n', n_2);
 fprintf('\n');
 
 % Bearing F
-fprintf('Mean diameter for bearing F: %.1f (mm)\n', dm_F);
-fprintf('RPM of shaft 3: %.1f (rpm)\n', n_4);
+fprintf('Mean diameter for bearing F: %.1f [mm]\n', dm_F);
+fprintf('RPM of shaft 3: %.1f [rpm]\n', n_4);
 fprintf('\n');
 
 % Bearing G
-fprintf('Mean diameter for bearing G: %.1f (mm)\n', dm_G);
-fprintf('RPM of shaft 3: %.1f (rpm)\n', n_4);
+fprintf('Mean diameter for bearing G: %.1f [mm]\n', dm_G);
+fprintf('RPM of shaft 3: %.1f [rpm]\n', n_4);
 fprintf('\n');
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 %% Functions 
 function ks = ks(F_t, b, d1, U)
     % Beregner k_s til Belastning-hastighetsfaktorer
     % Input:
-    %   Ft - Tannhjulskraft (N)
-    %   b  - Tannhjulsbredde (mm)
-    %   d1 - Delingssirkelens diameter (mm)
-    %   U  - Utveksling
+    %   Ft - Tangential Gear Force [N]
+    %   b  - Gear Width [mm]
+    %   d1 - Pitch Circle Diameter [mm]
+    %   U  - Gear Ratio
 
     % Output:
-    %   ks - (N/mm^2)
+    %   ks - [N/mm^2]
     
     ks = (F_t / (b * d1)) * ((U + 1) / U) * 3;
 end
